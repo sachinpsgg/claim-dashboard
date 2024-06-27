e31cda96c7c12c43d33a919f31417333d5f2e373
Neeraj Binani <neerajbinani@outlook.com>
2024-06-28 00:22:10 +0530

    login added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
 import logo from './logo.svg';
 import './App.scss';
+import Login from './Pages/Login/login';
+import './sass/style.scss';
 
 function App() {
   return (
-    <div className="bg-amber-800">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Login />
   );
 }
 
