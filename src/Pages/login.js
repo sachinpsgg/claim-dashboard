23f0821ca7151c6c59864babb4b590ee16537078
Neeraj Binani <neerajbinani@outlook.com>
2024-07-03 00:04:12 +0530

    login css fixed

--- src/Pages/login.js
+++ src/Pages/login.js
@@ -64,13 +64,13 @@ export default function Login({ setUser }) {
                                                             required="" />
                                                     </div>
                                                     <div className="d-flex align-items-center justify-content-between mt-4 mb-1">
-                                                        <div className="flex items-start">
-                                                            <div className="flex items-center h-5">
+                                                        <div className="d-flex align-items-start">
+                                                            <div className="d-flex align-items-center h-5">
                                                                 <input id="remember"
-                                                                    aria-describedby="remember" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required="" />
+                                                                    aria-describedby="remember" type="checkbox" className="" required="" />
                                                             </div>
-                                                            <div className="ml-3 text-sm">
-                                                                <label for="remember" className="text-gray-500 dark:text-gray-300">Remember me</label>
+                                                            <div className="ml-3">
+                                                                <label for="remember" className="">Remember me</label>
                                                             </div>
                                                         </div>
                                                         <a href="#" className="login-page--container--login-form--form-content--box--forgot-password">Forgot password?</a>
